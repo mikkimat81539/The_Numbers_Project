4cf3e61a71ffc2bb226fef934a92e42e4252e3e0
mikkimat81539 <mmatthews81539@gmail.com>
2026-09-25 13:43:11 -0500

    added random number pick and win/lose page

--- numberOne/PICK_A_NUMBER/script.js
+++ numberOne/PICK_A_NUMBER/script.js
@@ -1,12 +1,40 @@
 const main_div = document.getElementById("main")
 
-for (let i = 0; i < 100; i++){
-	// create a new div element
-	const newDiv = document.createElement("div");
+cpu_pick = Math.floor(Math.random() * 100) + 1; // random number
 
-	// add content
-	newDiv.textContent = i+1
+// This function makes a div element
+function makeStuff(){
+	for (let i = 0; i < 100; i++){
+		// create a new div element
+		const newDiv = document.createElement("div");
 
-	main_div.appendChild(newDiv)
+		// add content
+		newDiv.textContent = i+1
+
+		newDiv.addEventListener("click", num_select);
+
+		main_div.appendChild(newDiv)
+
+	}
+}
+
+console.log(cpu_pick)
+
+// when div is clicked pick number
+function num_select(event){
+	const num = event.currentTarget.textContent
+	
+	if (num == cpu_pick){
+		console.log("YOU WIN")
+		window.location.href = "win.html"
+	}
+
+	else {
+		console.log("YOU LOSE")
+		window.location.href = "lose.html"
+	}
 
 }
+
+makeStuff() // call the function
+
